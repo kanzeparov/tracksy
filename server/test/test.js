4b0c3faadb09b7a77168062a1f1ccd5f8779c295
kanzeparov <kanzeparov@yandex.ru>
2021-05-09 16:58:31 +0300

    added ! to test

--- server/test/test.js
+++ server/test/test.js
@@ -3,6 +3,6 @@ const app = require("../index");
 
 describe("GET /", () => {
     it("respond with Hello World", (done) => {
-        request(app).get("/").expect("Hello World via Github Actions", done);
+        request(app).get("/").expect("Hello! World via Github Actions", done);
     })
 });
