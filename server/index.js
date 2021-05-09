58ba431d4b02cb5048fb5975bc670d617548fe04
kanzeparov <kanzeparov@yandex.ru>
2021-05-09 17:30:14 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const app = express();
 app.get("/", function (req, res) {
     return res.send("Hello! World via Github Actions");
 });
+
 // listen to port 7000 by default
 app.listen(process.env.PORT || 7000, () => {
     console.log("Server is running");
