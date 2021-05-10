ac4b547bacc8faf06fb68a298dc20044bc055961
kanzeparov <kanzeparov@yandex.ru>
2021-05-10 11:37:02 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Respond with "hello world" for requests that hit our root "/"
 app.get("/", function (req, res) {
-    return res.send("Hello");
+    return res.send("Hello world");
 });
 
 // listen to port 7000 by default
