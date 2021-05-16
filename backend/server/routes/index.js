67ffefa5276352354dfe0a92f01e7e474c24b03c
Andrey <ScootalooRainbow@yandex.ru>
2021-05-16 23:06:45 +0900

    Auth done

--- backend/server/routes/index.js
+++ backend/server/routes/index.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
-const { createUser } = require('../controllers/auth');
+const { createUser, loginUser } = require('../controllers/auth');
 
 const router = Router();
 
 router.post('/signup', createUser);
+router.post('/signin', loginUser);
 
 module.exports = router;
